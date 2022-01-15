4d8f02ea18f3ca3b7d4d6689b5c1dbbb5a5e34ee
chao <22640673+chao7150@users.noreply.github.com>
2022-01-15 18:22:06 +0900

    first commit

---
+++ test/watch-duty-crawler.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WatchDutyCrawler from '../lib/watch-duty-crawler-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/watch-duty-crawler-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WatchDutyCrawler.WatchDutyCrawlerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
