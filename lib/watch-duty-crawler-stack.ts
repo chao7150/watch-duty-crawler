69c4595f4f2e6f44863c1611f8deeabab5c9f9c7
chao <22640673+chao7150@users.noreply.github.com>
2022-01-15 19:17:38 +0900

    can deploy ts function

--- lib/watch-duty-crawler-stack.ts
+++ lib/watch-duty-crawler-stack.ts
@@ -1,10 +1,12 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_lambda_nodejs } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class WatchDutyCrawlerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const func = new aws_lambda_nodejs.NodejsFunction(this, "crawler")
 
     // The code that defines your stack goes here
 
