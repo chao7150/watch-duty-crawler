57566de90de9952bb062304d2c5509490f87b475
chao <22640673+chao7150@users.noreply.github.com>
2022-02-10 01:41:33 +0900

    vpc and webserver

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -1,49 +1,54 @@
-import { Stack, StackProps, aws_ec2, aws_rds, aws_ecs } from "aws-cdk-lib";
+import { Stack, StackProps, aws_ec2 } from "aws-cdk-lib";
+import { KeyPair } from "cdk-ec2-key-pair";
 import { Construct } from "constructs";
-import * as keypair from "cdk-ec2-key-pair";
 
 export class VpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = new aws_ec2.Vpc(this, "vpc");
+    const vpc = new aws_ec2.Vpc(this, "vpc", {
+      cidr: "10.0.0.0/24",
+      maxAzs: 1,
+      natGateways: 0,
+      subnetConfiguration: [
+        { name: "web", subnetType: aws_ec2.SubnetType.PUBLIC },
+        { name: "db", subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED },
+      ],
+    });
 
-    // const cluster = new aws_ecs.Cluster(this, "Cluster", {
-    //   vpc,
-    // });
+    const securityGroup = new aws_ec2.SecurityGroup(this, "Security Group", {
+      vpc,
+      allowAllOutbound: true,
+    });
+    // Allow SSH access on port tcp/22
+    securityGroup.addIngressRule(
+      aws_ec2.Peer.anyIpv4(),
+      aws_ec2.Port.tcp(22),
+      "Allow SSH Access"
+    );
 
-    // // Add capacity to it
-    // cluster.addCapacity("DefaultAutoScalingGroupCapacity", {
-    //   instanceType: aws_ec2.InstanceType.of(
-    //     aws_ec2.InstanceClass.T2,
-    //     aws_ec2.InstanceSize.MEDIUM
-    //   ),
-    //   desiredCapacity: 3,
-    // });
+    // Allow HTTP access on port tcp/80
+    securityGroup.addIngressRule(
+      aws_ec2.Peer.anyIpv4(),
+      aws_ec2.Port.tcp(80),
+      "Allow HTTP Access"
+    );
 
-    // const taskDefinition = new aws_ecs.Ec2TaskDefinition(this, "TaskDef");
+    const key = new KeyPair(this, "ssh key pair", {
+      name: "watch-duty-manager",
+      storePublicKey: true, // by default the public key will not be stored in Secrets Manager
+    });
 
-    // taskDefinition.addContainer("DefaultContainer", {
-    //   image: aws_ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-    //   memoryLimitMiB: 512,
-    // });
-
-    // // Instantiate an Amazon ECS Service
-    // const ecsService = new aws_ecs.Ec2Service(this, "Service", {
-    //   cluster,
-    //   taskDefinition,
-    // });
-
-    // const rds = new aws_rds.DatabaseInstance(this, "DBInstance", {
-    //   engine: aws_rds.DatabaseInstanceEngine.mysql({
-    //     version: aws_rds.MysqlEngineVersion.VER_8_0_26,
-    //   }),
-    //   instanceType: aws_ec2.InstanceType.of(
-    //     aws_ec2.InstanceClass.T2,
-    //     aws_ec2.InstanceSize.MEDIUM
-    //   ),
-    //   vpc,
-    //   vpcSubnets: { subnetType: aws_ec2.SubnetType.PRIVATE_WITH_NAT },
-    // });
+    const ec2 = new aws_ec2.Instance(this, "web server", {
+      instanceType: aws_ec2.InstanceType.of(
+        aws_ec2.InstanceClass.T2,
+        aws_ec2.InstanceSize.MICRO
+      ),
+      vpc,
+      machineImage: aws_ec2.MachineImage.latestAmazonLinux(),
+      vpcSubnets: { subnetType: aws_ec2.SubnetType.PUBLIC },
+      securityGroup,
+      keyName: key.keyPairName,
+    });
   }
 }
